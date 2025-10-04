3373df8bdccc51505e38ab79783cf64613f157f9
andy <landywl4@gmail.com>
2025-10-03 23:21:10 -0700

    more mapping

--- scenes/MainCharacter/main_character.gd
+++ scenes/MainCharacter/main_character.gd
@@ -7,20 +7,20 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(_delta: float) -> void:
-    var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-    velocity = direction * SPEED
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED
 
-    if direction.y > 0:
-        animated_sprite.play("walk_down")
+	if direction.y > 0:
+		animated_sprite.play("walk_down")
 
-    if direction.y < 0:
-        animated_sprite.play("walk_up")
+	if direction.y < 0:
+		animated_sprite.play("walk_up")
 
-    if direction.x != 0 and direction.y == 0:
-        animated_sprite.play("walk_sides")
-        animated_sprite.flip_h = direction.x < 0
+	if direction.x != 0 and direction.y == 0:
+		animated_sprite.play("walk_sides")
+		animated_sprite.flip_h = direction.x < 0
 
-    if direction == Vector2.ZERO:
-        animated_sprite.play("idle")
+	if direction == Vector2.ZERO:
+		animated_sprite.play("idle")
 
-    move_and_slide() 
+	move_and_slide() 
